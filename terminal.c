1f14f34a4de7662a9d0bf0c46260caad4b89cacc
Gustavo Volobueff <volobueffgustavo@gmail.com>
2024-04-18 09:04:29 -0400

    added process tree

--- terminal.c
+++ terminal.c
@@ -1,3 +1,9 @@
+//Autor = Gustavo Kermaunar Volobueff
+//rgm47006
+//volobueffgustavo@gmail.com
+//Disciplina de Sistemas Operacionais
+//Prof. Dr. Fabrício Sérgio de Paula
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -19,7 +25,7 @@ struct TreeNode {
 	struct TreeNode *next;
 };
 
-static struct TreeNode *head;
+static struct TreeNode *processTree;
 
 //Insere um novo nó para a árvore de processos.
 void insertNode(char *proc_name, pid_t pid, pid_t ppid){
@@ -36,91 +42,154 @@ void insertNode(char *proc_name, pid_t pid, pid_t ppid){
     node->ppid = ppid;
     node->children[0] = NULL;
     node->parent = NULL;
-    node->next = head;
+    node->next = processTree;
+    processTree = node;
 }
 
-// pid_t getPPid(char *dir){
-//     char buf[MAXBUF];
-//     FILE *p_file = fopen(dir, "r");
-//     pid_t ppid;
-//     unsigned short i=0;
-//     char *token;
+pid_t getPPid(char *dir){
+    char buf[MAXBUF];
+    FILE *p_file = fopen(dir, "r");
+    pid_t ppid;
+    unsigned short i=0;
+    char *token;
 
-//     size_t ret = fread(buf, sizeof(char), MAXBUF-1, p_file);
-//     buf[ret++] = '\0';
+    //Abre o stat para pegar o ppid
+    size_t ret = fread(buf, sizeof(char), MAXBUF-1, p_file);
+    buf[ret++] = '\0';
 
-//     fclose(p_file);
+    fclose(p_file);
 
-//     token = strtok(buf, " ");
+    //Conta até o quarto token para pegar o quarto argumento (ppid)
+    token = strtok(buf, " ");
+    while(i<3){
+        token = strtok(NULL, " ");
+        i++;
+    }
 
-//     printf("%s\n", dir);
-//     while(i<4){
-//         printf("%s\n", token);
-//         token = strtok(NULL, " ");
-//         i++;
-//     }
-//     printf("%s\n", token);
-//     ppid = atoi(token);
+    ppid = atoi(token);
+    return ppid;
+}
 
-//     if(ppid == 0)
-//         exit(0);
+char* getProcName(char *dir, char *pid){
+    char buf[MAXBUF];
+    FILE *p_file = fopen(dir, "r");
+    unsigned short i=0;
+    char *token;
 
-//     return ppid;
-// }
+    //Abre o stat para pegar o ppid
+    size_t ret = fread(buf, sizeof(char), MAXBUF-1, p_file);
+    buf[ret++] = '\0';
+
+    fclose(p_file);
+
+    //Conta até o quarto token para pegar o quarto argumento (ppid)
+    token = strtok(buf, " ");
+    while(i<1){
+        token = strtok(NULL, " ");
+        i++;
+    }
+
+    return token;
+}
+
+//Acha um nó na lista encadeada formada para construir a árvore
+struct TreeNode* findNode(pid_t pid){
+    struct TreeNode *node;
+    
+	for (node = processTree; node != NULL; node = node->next) {
+		if (node->pid == pid)
+			return node;
+	}
+	return NULL;
+}
 
 //Comando tree PID
 void treeProcess(char *pid_path){
-    char *proc_path;
+    char proc_path[MAX_ARG_SIZE], proc_name[MAX_ARG_SIZE];
     DIR *proc_dir;
     struct dirent *entity;
     pid_t pid = atoi(pid_path), ppid;   
     unsigned cont_dir_task=0;
-
+    
     //Verificação se o processo existe
     strcpy(proc_path, "/proc/");
     strcat(proc_path, pid_path);
-    proc_dir = opendir(proc_path);
 
+    proc_dir = opendir(proc_path);    
     if(!proc_dir){
         printf("PID nao encontrado!\n");
         return;
     }
 
-    strcat(proc_path, "/task/");
-
+    strcat(proc_path, "/stat");
+    strcpy(proc_name, getProcName(proc_path, pid_path));
+    insertNode(proc_name, pid, 0);
+    
+    strcpy(proc_path, "/proc/");
     proc_dir = opendir(proc_path);
+    
+    while(entity = readdir(proc_dir)){
+        if(atoi(entity->d_name) > pid){
+            strcat(proc_path, entity->d_name);
+            strcat(proc_path, "/stat");
+            
+            ppid = getPPid(proc_path);
+            strcpy(proc_name, getProcName(proc_path, entity->d_name));
 
-    while(entity = readdir(proc_dir))
-        cont_dir_task++;
+            if(ppid == pid && !findNode(ppid)){
+                insertNode(proc_name, atoi(entity->d_name), ppid);
+                treeProcess(entity->d_name);
+            }
 
-    if(cont_dir_task > 3){ //Caso os nomes dos diretórios sejam os filhos
-        
-    }
-    else{ //Caso esteja no arquivo children
-        strcat(proc_path, pid_path);
-        
-        
-
+            memset(proc_path, '\0', sizeof(proc_path));
+            strcpy(proc_path, "/proc/");
+        }
     }
 
     closedir(proc_dir);
-
-    // strcpy(proc_path, "/proc/");
-    // proc_dir = opendir(proc_path);
-
-    
-    // while(entity = readdir(proc_dir)){
-    //     if(atoi(entity->d_name) > pid){
-    //         strcat(proc_path, entity->d_name);
-    //         strcat(proc_path, "/stat");
-
-    //         ppid = checkPPid(proc_path);
-
-    //         memset(proc_path, '\0', sizeof(proc_path));
-    //         strcpy(proc_path, "/proc/");
-    //     }
-    // }
 }   
+
+void buildTree(){
+	int i;
+	struct TreeNode *node, *pnode;
+
+	/* now we have a valid linked list, make a tree */
+	for(node = processTree; node != NULL; node = node->next) {
+		i = 0;
+
+		pnode = findNode(node->ppid);
+
+		if(pnode != NULL) {
+			node->parent = pnode;
+
+			while (pnode->children[i++] != NULL);
+
+			pnode->children[i - 1] = node;
+			pnode->children[i] = NULL;
+		}
+	}
+}
+
+//Imprime a árvore de processos
+void seeTree(struct TreeNode * root, int level){
+	int i;
+	struct TreeNode *node;
+
+	for (i = 0; i < level; i++) 
+		printf("  ");
+
+	printf("%s pid: %d, ppid: %d\n", root->name, root->pid, root->ppid);
+
+	//Recursão nos filhos
+	int j = 0;
+	while((node = root->children[j++]) != NULL)
+		seeTree(node, level + 1);
+}
+
+//Limpa os ponteiros para a próxima árvore
+void clearTree(){
+
+}
 
 int main(){
     char command[MAX_ARGS];
@@ -162,8 +231,18 @@ int main(){
 
         if(strcmp(args[0], "cd") == 0) //Comando cd pois o execve não o identifica
             chdir(args[1]);
-        else if(strcmp(args[0], "tree") == 0)
+        else if(strcmp(args[0], "tree") == 0){
             treeProcess(args[1]);
+            buildTree();
+
+            struct TreeNode *node;
+            for (node = processTree; node != NULL; node = node->next) {
+                if (node->parent == NULL)
+                    seeTree(node, 0);
+            }
+
+            clearTree();
+        }
         else{
             pid = fork();
 
