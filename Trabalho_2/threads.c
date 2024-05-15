914e35a0fee2c6ca03ceab69eac530397d732bc5
Gustavo Volobueff <volobueffgustavo@gmail.com>
2024-05-15 13:28:50 -0400

    almost done threads

--- Trabalho_2/threads.c
+++ Trabalho_2/threads.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include <math.h>
 
-long int sum;
-int *integers, numIntegers=0;
+long int sum=0, *arraySums;
+int *integers, numIntegers=0, indexSum=0;
 
 void *runner(void *param);
 void readFile(char*);
 
+/* Guarda, para cada thread, o inicio e o fim do array integers para somar */
 typedef struct{
 	int start, end;
 }ThreadsSubArrays;
@@ -32,6 +33,10 @@ int main(int argc, char *argv[]){
 	int nThreads = atoi(argv[2]);
 	pthread_t tid[nThreads];
 	ThreadsSubArrays subArraysIndices[nThreads];
+	arraySums = malloc (sizeof(int) * nThreads);
+
+	for(int i=0;i<nThreads;i++)
+		arraySums[i] = 0;
 
 	readFile(archiveName);
 
@@ -43,40 +48,54 @@ int main(int argc, char *argv[]){
 
 		free(archiveName);
 		free(integers);
+		free(arraySums);
 		return 0;
 	}
 
-	int quantPerSubArray = ceil(numIntegers / nThreads);
-
+	int quantPerSubArray = (int)(numIntegers / nThreads);
+	
+	/* Calcula as posições dos subarrays para as threads somá-las */
 	subArraysIndices[0].start = 0;
 	subArraysIndices[0].end = quantPerSubArray-1;
 	for(int i=1;i<nThreads;i++){
-		subArraysIndices[i].start = subArraysIndices[i-1].start+1;
-		subArraysIndices[i].end = subArraysIndices[i].start * quantPerSubArray;
-	}
+		subArraysIndices[i].start = subArraysIndices[i-1].end+1;
 
-	// /* Cria as threads */
-    // for (int i = 0; i < nThreads; i++)
-    // 	pthread_create(&tid[nThreads], NULL, &runner, &integers[i]);
+		if(subArraysIndices[i].start + quantPerSubArray-1 >= numIntegers)
+			subArraysIndices[i].end = numIntegers-1;
+		else
+			subArraysIndices[i].end = subArraysIndices[i].start + quantPerSubArray-1;
+	}
+	if(subArraysIndices[nThreads-1].end < numIntegers)
+		subArraysIndices[nThreads-1].end = numIntegers-1;
+
+	/* Cria as threads */
+    for (int i = 0; i < nThreads; i++){
+		indexSum = i;
+    	pthread_create(&tid[i], NULL, runner, &subArraysIndices[i]);
+	}
     
-	// /* Espera o thread ser encerrado */
-    // for (int i = 0; i < nThreads; i++)
-	//     pthread_join(tid[nThreads], NULL);
+	/* Espera o thread ser encerrado */
+    for (int i = 0; i < nThreads; i++)
+	    pthread_join(tid[i], NULL);
+
+	/* Soma os resultados dos subarrays */
+	for(int i=0;i<nThreads;i++)
+		sum += arraySums[i];
+
+	printf("Sum = %ld\n", sum);
 
 	free(archiveName);
 	free(integers);
+	free(arraySums);
 	return 0;
 }
 
-/* O thread assumirá o controle nessa função */
+/* As threads assumirão o controle nessa função */
 void *runner(void *param){
-	int i, upper = atoi(param);
-    int indice = *((int*)param);
+	ThreadsSubArrays *threads = (ThreadsSubArrays*)param;
 	
-	sum = 0;
-	
-	for (i = 1; i <= indice; i++)
-		sum += i;
+	for (int i = threads->start; i < threads->end; i++)
+		arraySums[indexSum] += integers[i];
 
 	pthread_exit(0);
 }
