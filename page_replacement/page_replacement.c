8b340cd8f55aa6da7988572146022c84d0b2942f
Gustavo Volobueff <volobueffgustavo@gmail.com>
2024-10-04 15:20:48 -0400

    some adds

--- page_replacement/page_replacement.c
+++ page_replacement/page_replacement.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp);
-void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp);
-void LRU(unsigned int *access_addr, unsigned int quant_elem, FILE *fp);
+void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, unsigned int *pages, unsigned int pages_size);
+void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, unsigned int *pages, unsigned int pages_size);
+void LRU(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, unsigned int *pages, unsigned int pages_size);
 int search_array(unsigned int *arr, unsigned int x, unsigned int arr_size); 
 
 int main(int argc, char *argv[]){
@@ -34,6 +34,8 @@ int main(int argc, char *argv[]){
     stat(arch_name, &st);
     arch_size = st.st_size;
     unsigned int access_addr[arch_size], quant_elem=0, value;
+    unsigned int pages_size = (int)(mem_size/page_size*1.0);
+    unsigned int pages[pages_size];
 
     fscanf(fp, "%d", &value);
     while(!feof(fp)){
@@ -43,16 +45,16 @@ int main(int argc, char *argv[]){
 
     fclose(fp);
 
-    fp = fopen("erros.out", "w");
+    fp = fopen("erros.txt", "w");   
 
     fwrite("FIFO: ", 1, sizeof("FIFO: "), fp);
-    FIFO(access_addr, quant_elem, &fp);
+    FIFO(access_addr, quant_elem, fp, pages, pages_size);
     
     fwrite("\n\nOPT: ", 1, sizeof("\n\nOPT: "), fp);
-    OPT(access_addr, quant_elem, &fp);  
+    OPT(access_addr, quant_elem, fp, pages, pages_size);  
 
     fwrite("\n\nLRU: ", 1, sizeof("\n\nLRU: "), fp);
-    LRU(access_addr, quant_elem, &fp);
+    LRU(access_addr, quant_elem, fp, pages, pages_size);
 
     fclose(fp);
 
@@ -67,14 +69,30 @@ int search_array(unsigned int *arr, unsigned int x, unsigned int arr_size){
     return -1;
 }
 
-void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp){
-    
+void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, unsigned int *pages, unsigned int pages_size){
+    unsigned int quant_errors=0, page_pos=0;
+    char to_write[7];
+
+    for(int i=0;i<quant_elem;i++){
+        if(search_array(pages, access_addr[i], pages_size) < 0 ){
+            sprintf(to_write, "%u - %u, ", access_addr[i], page_pos);
+            fwrite(to_write, 1, sizeof(to_write), fp);
+            quant_errors++;
+
+            if(page_pos > pages_size-1)
+                page_pos = 0;
+
+            pages[page_pos++] = access_addr[i];
+        }
+    }
+
+    printf("FIFO:\n     Quantidade de erros: %u\n     Percentual de erros: %.2f%%\n", quant_errors, ((100*quant_errors)/(quant_elem*1.0)));
 }
 
-void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp){
+void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, unsigned int *pages, unsigned int pages_size){
 
 }
 
-void LRU(unsigned int *access_addr, unsigned int quant_elem, FILE *fp){
+void LRU(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, unsigned int *pages, unsigned int pages_size){
 
 }
