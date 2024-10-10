51a019db7d4880fce8b1da8dd1837740efc06623
Gustavo Volobueff <volobueffgustavo@gmail.com>
2024-10-10 10:25:36 -0400

    fifo done

--- page_replacement/page_replacement.c
+++ page_replacement/page_replacement.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *pages, unsigned int pages_size, unsigned int each_page_byte);
-void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *pages, unsigned int pages_size, unsigned int each_page_byte);
-void LRU(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *pages, unsigned int pages_size, unsigned int each_page_byte);
+#define MAX_ARRAY_SIZE 65536
+
+void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *page_table, unsigned int quant_frames, unsigned int page_size);
+void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *page_table, unsigned int quant_frames, unsigned int page_size);
+// void LRU(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *pages, unsigned int pages_size);
 int search_array(int *arr, unsigned int x, unsigned int arr_size, unsigned int init); 
 void clearPages(int *pages, unsigned int pages_size);
 
@@ -18,12 +20,10 @@ int main(int argc, char *argv[]){
 
     unsigned int page_size = atoi(argv[1]);
     unsigned int mem_size = atoi(argv[2]);
-    char arch_name[sizeof(argv[3])];
+    char arch_name[sizeof(argv[3]) * sizeof(int)];
     strcpy(arch_name, argv[3]);
-
+    
     FILE *fp;
-    unsigned int arch_size;
-    struct stat st;
 
     fp = fopen(arch_name, "r");
 
@@ -31,23 +31,20 @@ int main(int argc, char *argv[]){
         printf("Archive %s not found.\n", arch_name);
         return -1;
     }
-
-    stat(arch_name, &st);
-    arch_size = st.st_size;
-    unsigned int access_addr[arch_size], quant_elem=0, value;
-    unsigned int pages_size = (int)(mem_size/page_size*1.0);
-    int pages[pages_size];
+    
+    int access_addr[MAX_ARRAY_SIZE], quant_elem=0, value, quant_frames = mem_size / page_size;
+    int page_table[quant_frames];
 
     //Inicialização do array com -1 pois pode ocorrer comparações com valores aleatórios.
-    for(int i=0;i<pages_size;i++)
-        pages[i] = -1;
-
+    for(int i=0;i<quant_frames;i++)
+        page_table[i] = -1;
+    
     fscanf(fp, "%d", &value);
     while(!feof(fp)){
         access_addr[quant_elem++] = value;
         fscanf(fp, "%d", &value);
     }
-
+    
     fclose(fp);
 
     fp = fopen("erros.txt", "r");
@@ -57,19 +54,19 @@ int main(int argc, char *argv[]){
 
     fclose(fp);
     fp = fopen("erros.txt", "a+");  
-
+    
     fprintf(fp, "%s", "FIFO:     ENDERECO     PAGINA");
-    FIFO(access_addr, quant_elem, fp, pages, pages_size, page_size);
-
-    clearPages(pages, pages_size);
+    FIFO(access_addr, quant_elem, fp, page_table, quant_frames, page_size);
+    
+    clearPages(page_table, quant_frames);
     
     fprintf(fp, "\n\n%s", "OPT:     ENDERECO     PAGINA");
-    OPT(access_addr, quant_elem, fp, pages, pages_size, page_size); 
+    OPT(access_addr, quant_elem, fp, page_table, quant_frames, page_size); 
 
-    clearPages(pages, pages_size); 
+    // clearPages(pages, pages_size); 
 
-    fprintf(fp, "\n\n%s", "LRU:     ENDERECO     PAGINA");
-    LRU(access_addr, quant_elem, fp, pages, pages_size, page_size);
+    // fprintf(fp, "\n\n%s", "LRU:     ENDERECO     PAGINA");
+    // LRU(access_addr, quant_elem, fp, pages, pages_size, page_size);
 
     fclose(fp);
 
@@ -90,18 +87,18 @@ int search_array(int *arr, unsigned int x, unsigned int arr_size, unsigned int i
     return -1;
 }
 
-void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *pages, unsigned int pages_size, unsigned int each_page_byte){
+void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *page_table, unsigned int quant_frames, unsigned int page_size){
     unsigned int quant_errors=0, page_pos=0;
 
     for(int i=0;i<quant_elem;i++){
-        if(search_array(pages, access_addr[i], pages_size, 0) < 0 ){
-            fprintf(fp, "\n              %u           %u", access_addr[i], i*each_page_byte);
+        if(search_array(page_table, (int)(access_addr[i]/page_size), quant_frames, 0) < 0){
+            fprintf(fp, "\n              %u           %u", access_addr[i], (int)(access_addr[i] / page_size));
+            
+            page_table[page_pos++] = (int)(access_addr[i]/page_size);
             quant_errors++; 
 
-            if(page_pos > pages_size-1)
+            if(page_pos >= quant_frames)
                 page_pos = 0;
-
-            pages[page_pos++] = access_addr[i];
         }
     }
 
@@ -109,45 +106,11 @@ void FIFO(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *pag
                    %u                   %.2f%%\n", quant_errors, ((100*quant_errors)/(quant_elem*1.0)));
 }
 
-void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *pages, unsigned int pages_size, unsigned int each_page_byte){
-    unsigned int quant_errors=0, page_pos=0, quant_elem_page=0;
-    int maior, verif=-1, init;
+void OPT(unsigned int *access_addr, unsigned int quant_elem, FILE *fp, int *page_table, unsigned int quant_frames, unsigned int page_size){
+    unsigned int quant_errors=0, page_pos=0;
 
     for(int i=0;i<quant_elem;i++){
-        if(search_array(pages, access_addr[i], pages_size, 0) < 0){
-            fprintf(fp, "\n              %u           %u", access_addr[i], i*each_page_byte);
-            quant_errors++;
-
-            if(quant_elem_page < pages_size){
-                pages[page_pos] = access_addr[i];
-                page_pos++;
-                quant_elem_page++;
-            }
-            else{
-                init = i+1;
-                maior = -1;
-                
-                for(int j=0;j<pages_size;j++){
-                    if((verif = search_array(access_addr, pages[j], quant_elem, init)) > maior || verif == -1){
-                        maior = verif;
-                        page_pos = j;
-
-                        if(verif == -1){
-                            maior = 1;
-                            break;
-                        }
-                    }
-                }
-
-                if(maior < 0)
-                    if(page_pos+1 == pages_size)
-                        page_pos = 0;
-                    else
-                        page_pos++;
-
-                pages[page_pos] = access_addr[i];
-            }
-        }
+        
     }
 
     printf("OPT:     QUANTIDADE DE ERROS     PERCENTUAL DE ERROS\n\
